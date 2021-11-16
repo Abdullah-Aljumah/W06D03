65391f01bb3077fb38ba89e285a9681d8bbb044e
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-16 18:29:32 +0300

    router update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,51 @@
-const express = require("express"); 
+const express = require("express");
 const app = express();
+const fs = require("fs");
 const PORT = 5000;
 
+let todo = [];
+app.use(express.json());
 
-app.get("/todo", (req,res)=> {
-    res.status(200).json(res)
-})
+fs.readFile("./db/todo.json", (err, data) => {
+  if (err) {
+    return err;
+  } else {
+    todo = JSON.parse(data.toString());
+  }
+});
 
+// Get all todo list
+app.get("/todo", (req, res) => {
+  res.status(200).json(todo);
+});
 
-
+// Update item
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+  console.log(task);
+  //   console.log(id);
+  let check = false;
+  todo.forEach((item) => {
+    if (item.id == id) {
+        // console.log(id);
+        // console.log(task);
+      item.task = task;
+      check = true;
+    }
+  });
+  if (check) {
+    fs.writeFile("./db/todo.json", JSON.stringify(todo), (err) => {
+      if (err) {
+        res.status(400).json("Bad input");
+      } else {
+        res.status(200).json(todo);
+      }
+    });
+  } else {
+    res.status(404).json("task not found");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
